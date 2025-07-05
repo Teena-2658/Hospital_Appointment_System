ff4de0ab0a7bdb31995df2fb54556070d1cee6a0
Teena Devray <teenadevray24@gmail.com>
2025-07-05 19:10:46 +0530

    final deploy2

--- server/server.js
+++ server/server.js
@@ -25,10 +25,10 @@ app.use("/api/users", userRoutes);
 app.use("/api/appointments", appointmentRoutes); // ✅ appointment route
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.use(express.static(path.join(__dirname, "../Client/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "Client", "dist", "index.html"));
   });
 }
 
